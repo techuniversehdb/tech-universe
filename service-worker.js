24bae59d9c4302bebd53eecedc94791b7ed2e55b
techuniversehdb <techuniversehdb@gmail.com>
2025-11-02 13:52:39 +0530

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = "tech-universe-v1";
+const urlsToCache = [
+  "index.html",
+  "styles.css",
+  "script.js",
+  "data.json",
+  "https://cdn-icons-png.flaticon.com/512/5968/5968871.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log("Caching assets");
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+      );
+    })
+  );
+});
